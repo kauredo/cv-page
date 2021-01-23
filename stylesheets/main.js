2b7f95c3da375aa6df96993389911c7ec25cd020
kauredo <vaskafig@gmail.com>
2021-01-23 19:01:41 +0000

    fix indentation

--- stylesheets/main.js
+++ stylesheets/main.js
@@ -1,28 +1,27 @@
 let root = document.documentElement;
 
 function changeColor(color) {
-  let root = document.documentElement;
-  let main = document.querySelector("#main-color");
-  let bg = document.querySelector("#background-color");;
-  
-  switch (color) {
-    case 'main' :
-      root.style.setProperty('--main-color', main.value);
-      break;
-    case 'bg' :
-      root.style.setProperty('--background-color', bg.value);
-      break;
-    default:
-      break;
-  }
+	let root = document.documentElement;
+	let main = document.querySelector('#main-color');
+	let bg = document.querySelector('#background-color');
+
+	switch (color) {
+		case 'main':
+			root.style.setProperty('--main-color', main.value);
+			break;
+		case 'bg':
+			root.style.setProperty('--background-color', bg.value);
+			break;
+		default:
+			break;
+	}
 }
 
-
 function openBurger() {
-  const burger = document.querySelector("#nav-icon1");
-  const logo = document.querySelector("#main-logo");
-  const nav = document.querySelector("#navbar");
-  burger.classList.toggle('open');
-  logo.classList.toggle('hidden');
-  nav.classList.toggle('reverse');
+	const burger = document.querySelector('#nav-icon1');
+	const logo = document.querySelector('#main-logo');
+	const nav = document.querySelector('#navbar');
+	burger.classList.toggle('open');
+	logo.classList.toggle('hidden');
+	nav.classList.toggle('reverse');
 }
