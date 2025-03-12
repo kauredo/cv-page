01a72130dc12c6a4bb5b793d00a9c9d87c981893
kauredo <vaskafig@gmail.com>
2025-03-12 11:40:27 +0000

    CV

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,7 +4,7 @@
 <footer class="bg-gray-100 py-6 text-center text-sm text-gray-600 dark:bg-gray-900 dark:text-white">
 	<nav class="mx-auto flex max-w-4xl items-center justify-between px-6 py-4">
 		<a
-			href="/files/CV_VascoKF.pdf"
+			href="/files/cv_vascokf_2025.pdf"
 			target="_blank"
 			class=" hover:text-gray-800 dark:hover:text-gray-200"
 		>
