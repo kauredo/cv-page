69a92975a79532bd26482b391577e820f01bda24
kauredo <vaskafig@gmail.com>
2025-03-19 15:02:51 +0000

    fix canonical

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,24 +1,25 @@
 import type { MetaTagsProps } from 'svelte-meta-tags';
 
 export const load = ({ url }) => {
+	const baseUrl = 'https://www.vascokf.com';
 	const baseMetaTags = Object.freeze({
 		title: 'My Portfolio',
 		titleTemplate: '%s | VascoKF',
 		description:
 			"Vasco KF's personal developer portfolio showcasing his latest web projects, skills and experience.",
-		canonical: new URL(url.pathname, url.origin).href,
+		canonical: `${baseUrl}${url.pathname}`,
 		openGraph: {
 			type: 'website',
-			url: new URL(url.pathname, url.origin).href,
+			url: `${baseUrl}${url.pathname}`,
 			locale: 'en_IE',
 			title: 'VascoKF — Portfolio',
 			description: 'Showcasing my latest web projects, skills and experience.',
 			siteName: 'VascoKF',
 			images: [
 				{
-					url: 'https://www.vascokf.com/images/portfolio.webp',
+					url: `${baseUrl}/images/portfolio.webp`,
 					alt: 'VascoKF — Portfolio',
-					secureUrl: 'https://www.vascokf.com/images/portfolio.webp',
+					secureUrl: `${baseUrl}/images/portfolio.webp`,
 					type: 'image/webp'
 				}
 			]
