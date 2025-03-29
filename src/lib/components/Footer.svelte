1fee0be50be6b83505aa0b4d1567dee07c6d2af6
kauredo <vaskafig@gmail.com>
2025-03-29 11:19:24 +0000

    change /cv/print to /cv

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -8,7 +8,7 @@
 		class="mx-auto flex max-w-4xl flex-col gap-4 px-4 py-3 text-center sm:flex-row sm:items-center sm:justify-between sm:px-6 sm:text-left"
 	>
 		<a
-			href="/cv/print"
+			href="/cv"
 			class="bg-vasco-200 text-vasco-800 dark:bg-vasco-700 dark:text-vasco-100 rounded-lg px-4 py-2 text-sm font-semibold"
 			target="_blank"
 			rel="noopener noreferrer"
